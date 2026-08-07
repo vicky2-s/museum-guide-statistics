35ebedd6f1db107fa31d88add03169a75d573e9f
vicky2-s <2052740271@qq.com>
2026-08-07 21:16:44 +0800

    add sessions and commission

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let currentUser = null;
 
 let guides = [];
+let sessions = [];
 
 
 // 页面加载
@@ -169,6 +170,16 @@ document
 .oninput=calculate;
 
 
+document
+.getElementById("addSession")
+.onclick=addSession;
+
+
+document
+.getElementById("saveBtn")
+.onclick=saveRecord;
+
+
 
 }
 
@@ -178,18 +189,15 @@ document
 
 function calculate(){
 
-
-const count=
+const count =
 Number(
 document.getElementById("ticketCount").value
 )||0;
 
 
-
 const income=count*45;
 
 const commission=count*1.8;
-
 
 
 document
@@ -198,12 +206,194 @@ document
 income.toFixed(2);
 
 
-
 document
 .getElementById("commission")
 .innerText=
 commission.toFixed(2);
 
+
+calculateShare();
+
+}
+function calculateShare(){
+
+
+let checked=[];
+
+
+document
+.querySelectorAll(".dutyGuide")
+.forEach(item=>{
+
+if(item.checked){
+
+checked.push(item.value);
+
+}
+
+});
+
+
+let total =
+Number(
+document.getElementById("commission").innerText
+);
+
+
+
+let result="";
+
+
+guides.forEach(g=>{
+
+
+let money=0;
+
+
+if(checked.includes(g.name)
+&& checked.length>0){
+
+money=
+total/checked.length;
+
+}
+
+
+result+=`
+
+<p>
+${g.name}：
+${money.toFixed(2)} 元
+</p>
+
+`;
+
+
+});
+
+
+document.getElementById("summary")
+.innerHTML=result;
+
+
+}
+function addSession(){
+
+
+let div=document.createElement("div");
+
+div.className="session";
+
+
+div.innerHTML=`
+
+<select class="sessionGuide">
+
+${guides.map(g=>
+`
+<option>
+${g.name}
+</option>
+`
+).join("")}
+
+</select>
+
+
+<input 
+class="sessionTime"
+placeholder="时间">
+
+
+<input
+class="sessionPeople"
+type="number"
+placeholder="人数">
+
+
+<button onclick="this.parentElement.remove();updateSummary()">
+
+删除
+
+</button>
+
+`;
+
+
+
+document
+.getElementById("sessionList")
+.appendChild(div);
+
+
+}
+function updateSummary(){
+
+let stats={};
+
+
+guides.forEach(g=>{
+
+stats[g.name]={
+count:0,
+people:0
+};
+
+});
+
+
+
+document
+.querySelectorAll(".session")
+.forEach(row=>{
+
+
+let name=
+row.querySelector(".sessionGuide").value;
+
+
+let people=
+Number(
+row.querySelector(".sessionPeople").value
+)||0;
+
+
+stats[name].count++;
+
+stats[name].people+=people;
+
+
+});
+
+
+
+let html="";
+
+
+guides.forEach(g=>{
+
+html+=`
+
+<p>
+<b>${g.name}</b><br>
+
+讲解：
+${stats[g.name].count}
+场
+
+游客：
+${stats[g.name].people}
+人
+
+</p>
+
+`;
+
+});
+
+
+document.getElementById("summary")
+.innerHTML=html;
 
 
 }
